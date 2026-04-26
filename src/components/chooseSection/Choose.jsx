9638f8fc5b50d879b4ccb7b519d2ae70493de51a
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-26 16:34:30 +0530

    get started choose section json data crate

---
+++ src/components/chooseSection/Choose.jsx
@@ -0,0 +1,15 @@
+
+
+const Choose = () => {
+    return (
+        <section className="my-container my-[clamp(3rem,6vw,7.5rem)]">
+            
+<div>
+
+</div>
+
+        </section>
+    );
+};
+
+export default Choose;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../components/banner/Banner";
+import Choose from "../components/chooseSection/Choose";
 import Navbar from "../components/navbar/Navbar";
 import Rating from "../components/ratingSection/Rating";
 
@@ -10,6 +11,7 @@ const Home = () => {
       <div className="bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6]">
         <Rating />
       </div>
+      <Choose />
     </div>
   );
 };
