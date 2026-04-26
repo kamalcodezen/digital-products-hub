8413bf1c8861fba4b4574f207fde284df04a4411
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-26 22:20:10 +0530

    CTA section design complete

--- src/components/ctaSection/CTASection.jsx
+++ src/components/ctaSection/CTASection.jsx
@@ -2,11 +2,10 @@ import { Play } from "lucide-react";
 
 const CTASection = () => {
   return (
-    <section>
+    <section className="my-container">
       <div className="py-[clamp(3rem,6vw,7.5rem)] space-y-7">
-
         {/* heading content  */}
-        <h2 className="text-[clamp(2.75rem,4vw,2rem)] font-bold mb-2 text-center">
+        <h2 className="text-[clamp(2.75rem,5vw,2.75rem)] font-bold mb-2 text-center text-white">
           Ready to Transform Your Workflow?
         </h2>
         <p className="text-gray-300 text-center">
@@ -16,11 +15,12 @@ const CTASection = () => {
 
         {/* CTA button */}
         <div className="space-x-4  text-center">
-          <button className="btn bg-white bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6] rounded-full  bg-clip-text text-transparent font-medium shadow-xl border ">
-            <a href="#">Explore Products</a>
+          <button className="btn bg-white rounded-full border py-5 px-5 font-medium">
+            <span className="bg-gradient-to-r from-[#321ae9] to-[#8e40fa] bg-clip-text text-transparent">
+              Explore Products
+            </span>
           </button>
-          <button className="btn font-medium rounded-full border-[1.5px] border-[#4F39F6] shadow text-[#1b00e4]">
-            <Play />
+          <button className="btn font-medium rounded-full border-[1.5px] border-white border-[#4F39F6] bg-transparent shadow text-white py-5 px-7 ">
             <a href="#">View Pricing</a>
           </button>
         </div>
