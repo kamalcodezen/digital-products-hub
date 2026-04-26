cce4eb86c6dc74e62025228e863ac8a78b8c4047
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-26 13:03:52 +0530

    navbar design and responsive complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,8 +19,12 @@ const Navbar = () => {
       <div className="navbar bg-base-100 shadow-sm my-container">
         <div className="navbar-start">
           {/* Dropdown menu */}
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div className="dropdown ">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden "
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -39,20 +43,74 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-gradient-to-r from-[#4F39F6]  to-[#9B5CF6] text-white rounded-box z-1 mt-3 w-52 p-2 shadow space-y-2"
             >
               {link}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+
+          {/* logo name */}
+          <a className=" text-2xl font-semibold bg-gradient-to-r from-[#4934ef]  to-[#924af6] bg-clip-text text-transparent">
+            DigiTools
+          </a>
         </div>
 
         {/* center menu */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{link}</ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+
+        {/* navbar end */}
+        <div className="navbar-end gap-3">
+          {/* cart details */}
+          <div className="flex items-center justify-between gap-3 ">
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+              >
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    {" "}
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />{" "}
+                  </svg>
+                  <span className="badge badge-sm indicator-item">0</span>
+                </div>
+              </div>
+              <div
+                tabIndex={0}
+                className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+              >
+                <div className="card-body">
+                  <span className="text-lg font-bold">8 Items</span>
+                  <span className="text-info">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button className="btn btn-primary btn-block">
+                      View cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p className="font-medium hidden md:flex">Login</p>
+          </div>
+
+          {/*button  */}
+          <a className="btn font-medium rounded-full bg-linear-to-r  from-[#4934ef]  to-[#924af6] text-white">
+            Get Started
+          </a>
         </div>
       </div>
     </div>
