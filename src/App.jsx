ae1f3e32bf3865e10f908238b42d5ec1cc38f0d3
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-26 05:48:39 +0530

    component folder create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h2 className="text-7xl bg-red-400">hello</h2>
-
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../components/banner/Banner";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
