358112fdd5aba6cfbf5bd70ef12c15f78c3caf78
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-27 01:20:07 +0530

    product button toggle complete

--- src/components/allTools/ALLTools.jsx
+++ src/components/allTools/ALLTools.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 const ALLTools = () => {
+  const [selected, setSelected] = useState("allProduct");
+
   return (
     <section className="my-container">
       <div>
-        
         {/* heading */}
         <h2 className="text-[clamp(2.75rem,5vw,2.75rem)] font-bold mb-2 text-center ">
           Premium Digital Tools
@@ -13,11 +16,19 @@ const ALLTools = () => {
         </p>
 
         {/* CTA button  */}
-        <div className="space-x-1 mx-auto mt-6 flex items-center text-center  shadow-3xl  bg-gray-100 w-[200px] h-[50px] px-[7px] py-6 rounded-full">
-          <button className="btn bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6] rounded-full text-white font-medium  ">
+        <div className="space-x-1 mx-auto mt-6 flex items-center text-center  shadow-3xl  bg-gray-100 w-50 h-12.5 px-1.75 py-6 rounded-full">
+          {/* all product */}
+          <button
+            onClick={() => setSelected("allProduct")}
+            className={`btn rounded-full  font-medium duration-500 transition-all ${selected === "allProduct" ? "bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6] text-white " : "bg-white text-black"} `}
+          >
             Products
           </button>
-          <button className="btn bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6] rounded-full text-white font-medium px-[22px]">
+          {/* selected cart btn */}
+          <button
+            onClick={() => setSelected("selectedCart")}
+            className={`btn rounded-full font-medium px-5.5 duration-500 transition-all ${selected === "selectedCart" ? "bg-linear-to-r from-[#4F39F6]  to-[#9B5CF6] text-white  duration-500 transition-all" : "bg-white text-black"} `}
+          >
             Cart (2)
           </button>
         </div>
